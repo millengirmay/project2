23705cdb6a0051e9a6c238ad7e55eb8e83a20164
millengirmay <millenkahsaygirmay@gmail.com>
2024-11-08 23:59:23 -0800

    this code commited

---
+++ casting.java
@@ -0,0 +1,11 @@
+public class casting {
+    public static void main(String[] args) {
+      /*  int num=20;
+        double d=num;
+        System.out.println(d);          widening casting*/
+        double d=25.89;
+        int i=(int)d;
+        System.out.print(i);          //narrowing casting
+
+    }
+}
